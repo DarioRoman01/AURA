a572de1b0f3a2f76eaf77a86359e40a56354a223
Haizza1 <darioroman2001@gmail.com>
2021-05-04 10:30:10 -0400

    create utiliti function to recognise keywords

--- lpp/token.py
+++ lpp/token.py
@@ -4,7 +4,7 @@ from enum import (
     unique
 )
 
-from typing import NamedTuple
+from typing import NamedTuple, Dict
 
 @unique
 class TokenType(Enum):
@@ -29,3 +29,10 @@ class Token(NamedTuple):
 
     def __str__(self) -> str:
         return f'Type: {self.token_type}, Literal: {self.literal}'
+
+def lookup_token_type(literal: str) -> TokenType:
+    keywords: Dict[str, TokenType] = {
+        'var': TokenType.LET
+    }
+
+    return keywords.get(literal, TokenType.IDENT)
