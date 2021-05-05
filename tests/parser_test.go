6192e1448fc21ab3e2c3d2322f8a2c62c60f6bd9
Haizza1 <darioroman2001@gmail.com>
2021-05-05 15:58:26 -0400

    create tests for parser.

---
+++ tests/parser_test.go
@@ -0,0 +1,31 @@
+package test_test
+
+import (
+	"lpp/lpp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseProgram(t *testing.T) {
+	assert := assert.New(t)
+	source := "var x = 5;"
+	lexer := lpp.NewLexer(source)
+	parser := lpp.NewParser(lexer)
+	program := parser.ParseProgam()
+
+	if !assert.NotNil(program) {
+		t.Log("program is nil")
+		t.Fail()
+	}
+
+	if !assert.IsType(lpp.Program{}, program) {
+		t.Log("program type is not Program!")
+		t.Fail()
+	}
+
+	if !assert.Implements((*lpp.ASTNode)(nil), program) {
+		t.Log("program type does not implement ast node interface")
+		t.Fail()
+	}
+}
