6b9c701293171c46cd55bda9caad9d73df1d3a9b
Haizza1 <darioroman2001@gmail.com>
2021-05-05 11:21:40 -0400

    implementing repl.

---
+++ lpp/repl.go
@@ -0,0 +1,24 @@
+package lpp
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var EOF_TOKEN = Token{Token_type: EOF, Literal: ""}
+
+func StartRpl() {
+	reader := bufio.NewReader(os.Stdin)
+	for source, _ := reader.ReadString('\n'); source != "salir()"; {
+		lexer := NewLexer(source)
+
+		for token := lexer.NextToken(); token != EOF_TOKEN; {
+			fmt.Println(token.PrintToken())
+			token = lexer.NextToken()
+		}
+
+		source, _ = reader.ReadString('\n')
+
+	}
+}
