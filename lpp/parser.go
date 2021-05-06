daf255b81c483bd17802902e0f11845284f4262d
Haizza1 <darioroman2001@gmail.com>
2021-05-05 20:48:57 -0400

    added methods to parser.

--- lpp/parser.go
+++ lpp/parser.go
@@ -1,14 +1,64 @@
 package lpp
 
 type Parser struct {
-	lexer *Lexer
+	lexer        *Lexer
+	currentToken *Token
+	peekToken    *Token
 }
 
 func NewParser(lexer *Lexer) *Parser {
-	return &Parser{lexer: lexer}
+	return &Parser{
+		lexer:        lexer,
+		currentToken: nil,
+		peekToken:    nil,
+	}
 }
 
 func (p *Parser) ParseProgam() Program {
 	program := Program{Staments: []Statement{}}
+
+	for p.currentToken.Token_type != EOF {
+		statement := p.parseStament()
+		if statement != nil {
+			program.Staments = append(program.Staments)
+		}
+	}
 	return program
+}
+
+func (p *Parser) expepectedToken(tokenType TokenType) bool {
+	return true
+}
+
+func (p *Parser) advanceTokens() {
+
+}
+
+func (p *Parser) parseLetSatement() *LetStatement {
+	letStatemnt := NewLetStatement(*p.currentToken, nil, nil)
+
+	if !p.expepectedToken(IDENT) {
+		return nil
+	}
+
+	letStatemnt.name = NewIdentifier(*p.currentToken, p.currentToken.Literal)
+
+	if !p.expepectedToken(ASSING) {
+		return nil
+	}
+
+	// todo finish when i know how to parse expressions
+	for p.currentToken.Token_type != SEMICOLON {
+		p.advanceTokens()
+	}
+
+	return letStatemnt
+}
+
+func (p *Parser) parseStament() *Statement {
+	if p.currentToken.Token_type == LET {
+		return p.parseLetSatement()
+	}
+
+	return nil
 }
