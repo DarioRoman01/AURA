a29bd9f0a3d5033e42f500da395247aa32ac1611
Haizza1 <darioroman2001@gmail.com>
2021-05-27 17:56:22 -0400

    adding more builtins functions to the lenguage.

--- lpp/builtins.go
+++ lpp/builtins.go
@@ -1,7 +1,10 @@
 package lpp
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 	"unicode/utf8"
 )
 
@@ -11,6 +14,10 @@ func wrongNumberofArgs(found, actual int) string {
 
 func unsoportedArgumentType(objType string) string {
 	return fmt.Sprintf("argumento para longitud no valido, se recibio %s", objType)
+}
+
+func noRequiredArgs(funcName string, actual int) string {
+	return fmt.Sprintf("La funcion %s no recibe arguments pero se recibieron %d", funcName, actual)
 }
 
 func Longitud(args ...Object) Object {
@@ -25,6 +32,62 @@ func Longitud(args ...Object) Object {
 	return &Error{Message: unsoportedArgumentType(types[args[0].Type()])}
 }
 
+func Escribir(args ...Object) Object {
+	if len(args) != 1 {
+		return &Error{Message: wrongNumberofArgs(len(args), 1)}
+	}
+
+	switch node := args[0].(type) {
+
+	case *String:
+		str := fmt.Sprintf("%s\n", node.Inspect())
+		fmt.Println(str)
+
+	case *Number:
+		fmt.Println(node.Inspect())
+
+	case *Bool:
+		fmt.Println(node.Inspect())
+
+	default:
+		return &Error{Message: unsoportedArgumentType(types[node.Type()])}
+	}
+
+	return nil
+}
+
+func Recibir(args ...Object) Object {
+	if len(args) > 0 {
+		return &Error{Message: noRequiredArgs("recibir", len(args))}
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	str := scanner.Text()
+
+	return &String{Value: str}
+}
+
+func RecibirEntero(args ...Object) Object {
+	if len(args) > 0 {
+		return &Error{Message: noRequiredArgs("recibir", len(args))}
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	strInt := scanner.Text()
+
+	number, err := strconv.Atoi(strInt)
+	if err != nil {
+		return &Error{Message: "No se puede parsear como entero"}
+	}
+
+	return &Number{Value: number}
+}
+
 var BUILTINS = map[string]*Builtin{
-	"longitud": NewBuiltin(Longitud),
+	"longitud":       NewBuiltin(Longitud),
+	"escribir":       NewBuiltin(Escribir),
+	"recibir":        NewBuiltin(Recibir),
+	"recibir_entero": NewBuiltin(RecibirEntero),
 }
