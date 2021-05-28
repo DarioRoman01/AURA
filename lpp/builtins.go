e3ca8649c9b8d71863919c63dfd72c88a61c0359
Haizza1 <darioroman2001@gmail.com>
2021-05-28 10:49:22 -0400

    add builtins to cast integers and strings.

--- lpp/builtins.go
+++ lpp/builtins.go
@@ -68,6 +68,36 @@ func Recibir(args ...Object) Object {
 	return &Error{Message: unsoportedArgumentType("recibir", types[args[0].Type()])}
 }
 
+func castInt(args ...Object) Object {
+	if len(args) > 1 {
+		return &Error{Message: wrongNumberofArgs(len(args), 1)}
+	}
+
+	if arg, isString := args[0].(*String); isString {
+		number, err := strconv.Atoi(arg.Value)
+		if err != nil {
+			return &Error{Message: fmt.Sprintf("No se puede parsear como entero %s", arg.Value)}
+		}
+
+		return &Number{Value: number}
+	}
+
+	return &Error{Message: unsoportedArgumentType("recibir", types[args[0].Type()])}
+}
+
+func castString(args ...Object) Object {
+	if len(args) > 1 {
+		return &Error{Message: wrongNumberofArgs(len(args), 1)}
+	}
+
+	if arg, isNumber := args[0].(*Number); isNumber {
+		strInt := strconv.Itoa(arg.Value)
+		return &String{Value: strInt}
+	}
+
+	return &Error{Message: unsoportedArgumentType("recibir", types[args[0].Type()])}
+}
+
 func RecibirEntero(args ...Object) Object {
 	if len(args) > 1 {
 		return &Error{Message: wrongNumberofArgs(len(args), 1)}
@@ -108,4 +138,6 @@ var BUILTINS = map[string]*Builtin{
 	"recibir":        NewBuiltin(Recibir),
 	"recibir_entero": NewBuiltin(RecibirEntero),
 	"tipo":           NewBuiltin(Tipo),
+	"entero":         NewBuiltin(castInt),
+	"texto":          NewBuiltin(castString),
 }
