f3c84491adbca0f99733cbeac2696775d9fb9ca9
Haizza1 <darioroman2001@gmail.com>
2021-06-25 11:04:53 -0400

    add floats to operators evaluation.

--- src/evaluator/operators.go
+++ src/evaluator/operators.go
@@ -15,6 +15,12 @@ func evaluateInfixExpression(operator string, left obj.Object, right obj.Object)
 	case left.Type() == obj.FLOATING && right.Type() == obj.FLOATING:
 		return evaluateFloatInfixExpression(operator, left, right)
 
+	case left.Type() == obj.FLOATING && right.Type() == obj.INTEGERS:
+		return evaluateLeftFloatInfixExp(operator, left, right)
+
+	case left.Type() == obj.INTEGERS && right.Type() == obj.FLOATING:
+		return evaluateRigthFloatInfixExp(operator, left, right)
+
 	case left.Type() == obj.STRINGTYPE && right.Type() == obj.STRINGTYPE:
 		return evaluateStringInfixExpression(operator, left, right)
 
@@ -59,6 +65,92 @@ func evaluateBoolInfixExpression(operator string, left *obj.Bool, rigth *obj.Boo
 
 	case "!=":
 		return toBooleanObject(left != rigth)
+
+	default:
+		return unknownInfixOperator(
+			obj.Types[left.Type()],
+			operator,
+			obj.Types[rigth.Type()],
+		)
+	}
+}
+
+func evaluateLeftFloatInfixExp(operator string, left obj.Object, rigth obj.Object) obj.Object {
+	leftVal := left.(*obj.Float).Value
+	rigthVal := left.(*obj.Number).Value
+
+	switch operator {
+	case "+":
+		return &obj.Float{Value: leftVal + float64(rigthVal)}
+	case "-":
+		return &obj.Float{Value: leftVal - float64(rigthVal)}
+	case "*":
+		return &obj.Float{Value: leftVal * float64(rigthVal)}
+	case "/":
+		return &obj.Float{Value: leftVal / float64(rigthVal)}
+	case "+=":
+		left.(*obj.Float).Value += float64(rigthVal)
+		return left
+
+	case "-=":
+		left.(*obj.Float).Value -= float64(rigthVal)
+		return left
+
+	case "/=":
+		left.(*obj.Float).Value /= float64(rigthVal)
+		return left
+
+	case "*=":
+		left.(*obj.Float).Value *= float64(rigthVal)
+		return left
+
+	case ">":
+		return toBooleanObject(leftVal > float64(rigthVal))
+	case "<":
+		return toBooleanObject(leftVal < float64(rigthVal))
+	case "==":
+		return toBooleanObject(leftVal == float64(rigthVal))
+	case "!=":
+		return toBooleanObject(leftVal != float64(rigthVal))
+	case ">=":
+		return toBooleanObject(leftVal >= float64(rigthVal))
+	case "<=":
+		return toBooleanObject(leftVal <= float64(rigthVal))
+
+	default:
+		return unknownInfixOperator(
+			obj.Types[left.Type()],
+			operator,
+			obj.Types[rigth.Type()],
+		)
+	}
+}
+
+func evaluateRigthFloatInfixExp(operator string, left obj.Object, rigth obj.Object) obj.Object {
+	leftVal := left.(*obj.Number).Value
+	rigthVal := rigth.(*obj.Float).Value
+
+	switch operator {
+	case "+":
+		return &obj.Float{Value: float64(leftVal) + rigthVal}
+	case "-":
+		return &obj.Float{Value: float64(leftVal) - rigthVal}
+	case "*":
+		return &obj.Float{Value: float64(leftVal) * rigthVal}
+	case "/":
+		return &obj.Float{Value: float64(leftVal) / rigthVal}
+	case ">":
+		return toBooleanObject(float64(leftVal) > rigthVal)
+	case "<":
+		return toBooleanObject(float64(leftVal) < rigthVal)
+	case "==":
+		return toBooleanObject(float64(leftVal) == rigthVal)
+	case "!=":
+		return toBooleanObject(float64(leftVal) != rigthVal)
+	case ">=":
+		return toBooleanObject(float64(leftVal) >= rigthVal)
+	case "<=":
+		return toBooleanObject(float64(leftVal) <= rigthVal)
 
 	default:
 		return unknownInfixOperator(
@@ -120,7 +212,6 @@ func evaluateSuffixExpression(operator string, left obj.Object) obj.Object {
 func evaluateFloatInfixExpression(operator string, left, rigth obj.Object) obj.Object {
 	leftVal := left.(*obj.Float).Value
 	rigthVal := rigth.(*obj.Float).Value
-
 	switch operator {
 	case "+":
 		return &obj.Float{Value: leftVal + rigthVal}
@@ -224,12 +315,18 @@ func evaluateIntegerInfixExpression(operator string, left, rigth obj.Object) obj
 
 // check that the character after - is a number and apply the operator
 func evaluateMinusOperatorExpression(rigth obj.Object) obj.Object {
-	if right, isNumber := rigth.(*obj.Number); isNumber {
-		right.Value = -right.Value
-		return right
-	}
+	switch num := rigth.(type) {
+	case *obj.Number:
+		num.Value = -num.Value
+		return num
 
-	return unknownPrefixOperator("-", obj.Types[rigth.Type()])
+	case *obj.Float:
+		num.Value = -num.Value
+		return num
+
+	default:
+		return unknownPrefixOperator("-", obj.Types[rigth.Type()])
+	}
 }
 
 // evaluate prefix expressions
