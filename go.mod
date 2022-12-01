d024bb4e8651d5839a69c8f13b603273e447e35a
Dario Roman <darioroman2001@gmail.com>
2022-11-30 22:45:29 -0300

    add fiber

--- go.mod
+++ go.mod
@@ -5,10 +5,21 @@ go 1.18
 require github.com/stretchr/testify v1.7.0
 
 require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/gofiber/fiber/v2 v2.40.1 // indirect
 	github.com/jfeliu007/goplantuml v1.6.1 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.41.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- main.go
+++ main.go
@@ -5,52 +5,93 @@ import (
 	l "aura/src/lexer"
 	obj "aura/src/object"
 	p "aura/src/parser"
-	"encoding/json"
-	"html"
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
-func handleEvaluation(w http.ResponseWriter, r *http.Request) {
-	var body map[string]string
-	err := json.NewDecoder(r.Body).Decode(&body)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
+// func handleEvaluation(w http.ResponseWriter, r *http.Request) {
+// 	var body map[string]string
+// 	err := json.NewDecoder(r.Body).Decode(&body)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	source, ok := body["source"]
+// 	if !ok {
+// 		http.Error(w, "no source provided", http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	lexer := l.NewLexer(source)
+// 	parser := p.NewParser(lexer)
+// 	env := obj.NewEnviroment(nil)
+// 	program := parser.ParseProgam()
+
+// 	w.Header().Set("Content-Type", "application/json")
+// 	w.WriteHeader(http.StatusOK)
+
+// 	if len(parser.Errors()) > 0 {
+// 		json.NewEncoder(w).Encode(map[string][]string{
+// 			"errors": parser.Errors(),
+// 		})
+// 		// we dont evaluate the program if it has syntax errors
+// 		return
+// 	}
+
+// 	evaluated := e.Evaluate(program, env)
+// 	if evaluated != nil && evaluated != obj.SingletonNUll {
+// 		json.NewEncoder(w).Encode(map[string]string{
+// 			"result": evaluated.Inspect(),
+// 		})
+// 	}
+// }
+
+type Request struct {
+	Source string `json:"source"`
+}
+
+func handleEvaluation2(c *fiber.Ctx) error {
+	var body Request
+	if err := c.BodyParser(&body); err != nil {
+		fmt.Println(err)
+		return err
 	}
 
-	source, ok := body["source"]
-	if !ok {
-		http.Error(w, "no source provided", http.StatusBadRequest)
-		return
-	}
-
+	source := body.Source
 	lexer := l.NewLexer(source)
 	parser := p.NewParser(lexer)
 	env := obj.NewEnviroment(nil)
 	program := parser.ParseProgam()
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
 	if len(parser.Errors()) > 0 {
-		json.NewEncoder(w).Encode(map[string][]string{
+		return c.Status(http.StatusOK).JSON(map[string][]string{
 			"errors": parser.Errors(),
 		})
-		// we dont evaluate the program if it has syntax errors
-		return
 	}
 
 	evaluated := e.Evaluate(program, env)
 	if evaluated != nil && evaluated != obj.SingletonNUll {
-		json.NewEncoder(w).Encode(map[string]string{
-			"result": html.EscapeString(evaluated.Inspect()),
+		return c.Status(http.StatusOK).JSON(map[string]string{
+			"result": evaluated.Inspect(),
 		})
 	}
+
+	return c.Status(http.StatusOK).JSON(map[string]string{
+		"result": "no content",
+	})
 }
 
 func main() {
-	http.HandleFunc("/", handleEvaluation)
-	log.Println("Listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+	app.Post("/", handleEvaluation2)
+	log.Fatalf("Error: %s", app.Listen(":8080"))
 }
